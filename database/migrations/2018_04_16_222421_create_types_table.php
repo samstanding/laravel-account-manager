523e35f438ca659cf47707903397c6ede6b617a4
Sam Standing <samzstanding@gmail.com>
2018-04-16 17:31:14 -0500

    database tables created and migrated to db

--- database/migrations/2018_04_16_222421_create_types_table.php
+++ database/migrations/2018_04_16_222421_create_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAccountsTable extends Migration
+class CreateTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateAccountsTable extends Migration
      */
     public function up()
     {
-        Schema::create('accounts', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email');    
-            $table->integer('account_type_id');
-            $table->boolean('active');        
+            $table->string('account_type');
+            $table->text('description');
+            $table->integer('cost');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateAccountsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('accounts');
+        Schema::dropIfExists('types');
     }
 }
